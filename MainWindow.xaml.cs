f9d3ac7bf52eeae8e9c2d46fb6561b2dcdece1d5
Sahashirokov <90800840+Sahashirokov@users.noreply.github.com>
2023-01-05 23:27:59 +0500

    Release App Process version 1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Ressy;
+using Serilization;
+using static System.Diagnostics.Process;
+
+namespace Process
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _buffer;
+        Dictionary<string,double> Viewm = new Dictionary<string, double>();
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            
+        }
+        
+
+        private void Add_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "EXE files (*.exe)|*.exe";
+            if (openFileDialog.ShowDialog() == true)
+            {
+             
+                string buf = openFileDialog.FileName;
+                int v = buf.LastIndexOf(@"\");
+                buf = buf.Substring(v + 1);
+               
+                
+                string flippedText = new string(buf.Reverse().ToArray());
+                v = flippedText.LastIndexOf(@".");
+                buf = flippedText.Substring(v+1);
+                _buffer = new string(buf.Reverse().ToArray());
+                StackProcess stackProcess = new StackProcess
+                {
+                    Name = _buffer
+                };
+                stackProcess.CurrentProcess();
+            }
+           
+        }
+
+        private void Update_OnClick(object sender, RoutedEventArgs e)
+        {
+            ProcessCombobox.Items.Clear();
+            StackProcess stackProcess = new StackProcess();
+            stackProcess.Update();
+            Viewm = stackProcess.OpenLoad();
+            foreach (var item in Viewm)
+            {
+                ProcessCombobox.Items.Add(item.Key);
+            }
+        }
+
+        private void ProcessCombobox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            bool IsActiv = false;
+            string buf="";
+            
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+            
+            object selectedItem = ProcessCombobox.SelectedItem;
+            
+
+            if (selectedItem!=null)
+            {
+                StackProcess stackProcess = new StackProcess();
+                stackProcess.Update();
+                Viewm = stackProcess.OpenLoad();
+                foreach (var item in Viewm)
+                {
+                    if ((string) selectedItem ==item.Key )
+                    {
+                        if ((int)item.Value/60>=1)
+                        {
+                            TotalTime.Text =$"  {(int)item.Value/60} hour ";
+                        }
+                        else
+                        {
+                            TotalTime.Text =$"   {(int)item.Value} minute ";
+                        }
+                   
+                    }
+                
+                }
+                foreach (var item in processes)
+                {
+                
+                    if (item.ProcessName == selectedItem.ToString())
+                    {
+                        buf = "active";
+                        IsActiv = true;
+
+                    }
+               
+                }
+                if (!IsActiv)
+                {
+                    buf = "no active";
+                }
+                Block.Text = buf;
+            }
+            else
+            {
+                Block.Text = "";
+            }
+            
+            
+            
+        }
+    }
+}
---
+++ StackProcess.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace Serilization;
+
+public class StackProcess
+{ 
+    private const string PATH = "SaveProcess.JSON";
+    public string Name { get; set; }
+    private bool _isUpdate = false;
+    Dictionary<string, double> _procesAdd = new Dictionary<string, double>();
+
+        //Сохраняем Данные
+    private int ActivProcess()
+    {
+        bool isActiv = false;
+        double hour=0;
+        double minute=0;
+        System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
+        foreach (var item in processes)
+        {
+            if (item.ProcessName == Name)
+            {
+                isActiv = true;
+                break;
+            }
+        }
+
+        if (isActiv)
+        {
+            System.Diagnostics.Process proc = System.Diagnostics.Process.GetProcessesByName(Name)[0];
+            ProcessThreadCollection processThreads = proc.Threads;
+            foreach (ProcessThread thread in processThreads)
+            {
+                hour=(DateTime.Now - thread.StartTime).TotalHours;
+                minute=(DateTime.Now - thread.StartTime).TotalMinutes;
+                break;
+            }
+            
+            _isUpdate = true;
+        }
+
+        int m = (int) minute % 60;
+        return m;
+    }
+    public void CurrentProcess()
+    {
+       
+        Console.WriteLine(" Выбранный текущий процесс:");
+        _procesAdd.Add(Name,ActivProcess());
+        foreach (var item in _procesAdd)
+        {
+            Console.WriteLine($"    Название процесса - {item.Key}, Время текущего запуска - {item.Value}");
+        }
+        Defult();
+        SaveProcess();
+    }
+
+    public void Defult()
+    {
+        var fileExists = File.Exists(PATH);
+        if (!fileExists)
+        {
+            File.CreateText(PATH).Dispose();
+            StackProcess stackProcess = new StackProcess()
+            {
+                Name = "Defult"
+            };
+           SaveDefult();
+        }
+    }
+    private void SaveDefult()
+    {
+        
+        using (StreamWriter writer = File.CreateText(PATH))
+        {
+            string output = JsonConvert.SerializeObject(_procesAdd);
+            writer.Write(output);
+        }
+    }
+    private void SaveProcess()
+    {
+       // var keyValuePairs = _procesAdd.Concat(LoadProcess()!);
+       foreach (var item in LoadProcess()!)
+       {
+           _procesAdd[item.Key] = Convert.ToInt32(item.Value);
+       }
+
+        using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(_procesAdd);
+                writer.Write(output);
+            }
+    }
+    private Dictionary<string, double> LoadProcess()
+    {
+        Defult();
+        using (var reader = File.OpenText(PATH))
+        {
+           
+            var filetext = reader.ReadToEnd();
+            Dictionary<string, double>? procesLoad = JsonConvert.DeserializeObject<Dictionary<string,double>>(filetext);
+            return procesLoad;
+        }
+    }
+    //Обновляем данные
+    public void Update()
+    {
+        double minute=0;
+        double buffer=0;
+        bool IsActiv = false;
+       Dictionary<string,double> procupdate = LoadProcess();
+
+        for (int i = 0; i < LoadProcess().Count; i++)
+        {
+            try
+            {
+                var pair = LoadProcess().ElementAt(i);
+                
+                IsActiv = System.Diagnostics.Process.GetProcessesByName(pair.Key)[0].HasExited;
+               
+                if (!IsActiv)
+                {
+                    Console.WriteLine(pair.Key);
+                }
+               
+                System.Diagnostics.Process proc = System.Diagnostics.Process.GetProcessesByName(pair.Key)[0];
+                ProcessThreadCollection processThreads = proc.Threads;
+                foreach (ProcessThread thread in processThreads)
+                {
+                   
+                    minute = (DateTime.Now - thread.StartTime).TotalMinutes;
+                    buffer =  minute ;
+                    break;
+                }
+
+                if (Convert.ToInt32(pair.Value) != buffer)
+                {
+                        
+                        double col = Math.Abs(Math.Round(buffer) - Convert.ToInt32(pair.Value));
+                        pair = new KeyValuePair<string, double>(pair.Key, pair.Value+col);
+                        procupdate![pair.Key] = pair.Value; 
+                        
+                }
+                else
+                {
+                    pair = new KeyValuePair<string, double>(pair.Key, (int)minute % 60);
+                    procupdate![pair.Key] = pair.Value;
+                }
+
+            }
+            catch (Exception e) { }
+        }
+        
+        using (StreamWriter writer = File.CreateText(PATH))
+        {
+            string output = JsonConvert.SerializeObject(procupdate);
+            writer.Write(output);
+        }
+        
+    }
+    
+    public Dictionary<string,double> OpenLoad()
+    {
+        Console.WriteLine(" Сохраненые процессы: ");
+        foreach (var item in LoadProcess())
+        {
+            Console.WriteLine($"    Название процесса - {item.Key}, Общее время  запуска - {item.Value}");
+        }
+
+        return LoadProcess();
+    }
+}
