bf19f49912ee664d4a5671843ebb3ec168fe7da7
Sahashirokov <90800840+Sahashirokov@users.noreply.github.com>
2023-01-06 23:13:29 +0500

    Add Version "Launcher Process"2.0;

--- StackProcess.cs
+++ StackProcess.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Printing;
 using System.Text.RegularExpressions;
+using System.Windows.Input;
 using Newtonsoft.Json;
 
 namespace Serilization;
@@ -16,43 +18,15 @@ public static class NewStart
 public class StackProcess
 { 
     private const string PATH = "SaveProcess.JSON";
-    public string Name { get; set; }
+    public string? Name { get; set; }
     private bool _isUpdate = false;
     
-    Dictionary<string, double> _procesAdd = new Dictionary<string, double>();
+    Dictionary<string?, TimeSpan> _procesAdd = new ();
 
         //Сохраняем Данные
-    private int ActivProcess()
+    private TimeSpan ActivProcess()
     {
-        bool isActiv = false;
-        double hour=0;
-        double minute=0;
-        System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();
-        foreach (var item in processes)
-        {
-            if (item.ProcessName == Name)
-            {
-                isActiv = true;
-                break;
-            }
-        }
-
-        if (isActiv)
-        {
-            System.Diagnostics.Process proc = System.Diagnostics.Process.GetProcessesByName(Name)[0];
-            ProcessThreadCollection processThreads = proc.Threads;
-            foreach (ProcessThread thread in processThreads)
-            {
-                hour=(DateTime.Now - thread.StartTime).TotalHours;
-                minute=(DateTime.Now - thread.StartTime).TotalMinutes;
-                break;
-            }
-            
-            _isUpdate = true;
-        }
-
-        int m = (int) minute % 60;
-        return m;
+        return new TimeSpan(1);
     }
     public void CurrentProcess()
     {
@@ -82,7 +56,6 @@ public class StackProcess
     }
     private void SaveDefult()
     {
-        
         using (StreamWriter writer = File.CreateText(PATH))
         {
             string output = JsonConvert.SerializeObject(_procesAdd);
@@ -94,7 +67,7 @@ public class StackProcess
        // var keyValuePairs = _procesAdd.Concat(LoadProcess()!);
        foreach (var item in LoadProcess()!)
        {
-           _procesAdd[item.Key] = Convert.ToInt32(item.Value);
+           _procesAdd[item.Key] = (item.Value);
        }
 
         using (StreamWriter writer = File.CreateText(PATH))
@@ -103,93 +76,38 @@ public class StackProcess
                 writer.Write(output);
             }
     }
-    private Dictionary<string, double> LoadProcess()
+    private Dictionary<string?, TimeSpan> LoadProcess()
     {
         Defult();
         using (var reader = File.OpenText(PATH))
         {
-           
             var filetext = reader.ReadToEnd();
-            Dictionary<string, double>? procesLoad = JsonConvert.DeserializeObject<Dictionary<string,double>>(filetext);
+            Dictionary<string, TimeSpan>? procesLoad = JsonConvert.DeserializeObject<Dictionary<string,TimeSpan>>(filetext);
             return procesLoad;
         }
     }
     //Обновляем данные
-    public void Update()
+    public  virtual void  Update(string? Name,TimeSpan time)
     {
-        double minute=0;
-        double buffer=0;
-        bool IsActiv = false;
        
-        int count = 0;
-       Dictionary<string,double> procupdate = LoadProcess();
-
-        for (int i = 0; i < LoadProcess().Count; i++)
-        {
-            try
-            {
-                var pair = LoadProcess().ElementAt(i);
-                
-                IsActiv = System.Diagnostics.Process.GetProcessesByName(pair.Key)[0].HasExited;
-               
-                if (!IsActiv)
-                {
-                    Console.WriteLine(pair.Key);
-                }
-               
-                System.Diagnostics.Process proc = System.Diagnostics.Process.GetProcessesByName(pair.Key)[0];
-                ProcessThreadCollection processThreads = proc.Threads;
-                foreach (ProcessThread thread in processThreads)
-                {
-                   
-                    minute = (DateTime.Now - thread.StartTime).TotalMinutes;
-                    buffer =  minute ;
-                   
-                    break;
-                }
-                
-                if (Convert.ToInt32(pair.Value) != buffer)
-                {
-                    if (buffer<pair.Value)
-                    {
-                        NewStart._Buffer = pair.Value;
-                    }
-                    else
-                    {
-                        NewStart._Buffer = 0;
-                    }
-                    double col = Math.Abs( NewStart._Buffer+Math.Round(buffer) - Convert.ToInt32(pair.Value));
-                    col = Math.Abs(buffer - col);
-                    pair = new KeyValuePair<string, double>(pair.Key, pair.Value+col);
-                    procupdate![pair.Key] = pair.Value; 
-                        
-                }
-                else
-                {
-                    pair = new KeyValuePair<string, double>(pair.Key, (int)minute % 60);
-                    procupdate![pair.Key] = pair.Value;
-                }
-
-            }
-            catch (Exception e) { }
-        }
-        
+       Dictionary<string?, TimeSpan> CurrentUpdateTime = LoadProcess();
+       CurrentUpdateTime[Name] += time;
+       
         using (StreamWriter writer = File.CreateText(PATH))
         {
-            string output = JsonConvert.SerializeObject(procupdate);
+            string output = JsonConvert.SerializeObject(CurrentUpdateTime);
             writer.Write(output);
         }
         
     }
     
-    public Dictionary<string,double> OpenLoad()
+    public Dictionary<string?, TimeSpan> OpenLoad()
     {
         Console.WriteLine(" Сохраненые процессы: ");
         foreach (var item in LoadProcess())
         {
             Console.WriteLine($"    Название процесса - {item.Key}, Общее время  запуска - {item.Value}");
         }
-
         return LoadProcess();
     }
 }
